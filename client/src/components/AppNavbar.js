16e98f80e9af36eddc6e0744298121d1af40cc8e
Ryan Smith <ryan@Ryans-MBP.attlocal.net>
2020-09-25 08:04:22 -0500

    Adds front end portion of application, react and reactstrap

---
+++ client/src/components/AppNavbar.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import {
+    Collapse,
+    Navbar,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem,
+    NavbarText,
+    NavbarToggler,
+    Container
+} from 'reactstrap';
+
+class AppNavbar extends Component {
+    state = {
+        isOpen: false
+    }
+
+    toggle = () => {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
+    
+    render() {
+        return(
+            <div>
+            <Navbar color="dark" dark expand="sm" className="mb-5">
+                <Container>
+                    <NavbarBrand href="/">ShoppingList</NavbarBrand>
+                    <NavbarToggler onClick={this.toggle} />
+                    <Collapse isOpen={this.state.isOpen} navbar>
+                        <Nav className="ml-auto" navbar>
+                            <NavItem>
+                                <NavLink href="https://github.com/ryanlsmith142">
+                                    Github
+                                </NavLink>
+                            </NavItem>
+                        </Nav>
+                    </Collapse>
+                </Container>
+            </Navbar>
+        </div>
+        );
+    }
+
+}
+
+
+
+export default AppNavbar;
