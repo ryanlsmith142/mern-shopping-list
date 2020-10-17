d251b87d74622d0fd84e0b1c9b18a143090c97f7
Ryan Smith <ryan@Ryans-MBP.attlocal.net>
2020-10-16 20:07:09 -0500

    Adds modal that allows adding items to shopping list

--- client/src/App.js
+++ client/src/App.js
@@ -3,16 +3,20 @@ import AppNavbar from './components/AppNavbar';
 import ShoppingList from './components/ShoppingList'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-
+import ItemModal from './components/ItemModal';
 import { Provider } from 'react-redux';
 import store from './store';
+import { Container } from 'reactstrap';
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
         <AppNavbar />
-        <ShoppingList />
+        <Container>
+          <ItemModal />
+          <ShoppingList />
+        </Container>
       </div>
     </Provider>
   );
